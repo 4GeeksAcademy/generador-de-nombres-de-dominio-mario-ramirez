f22a59e16cb458e6c90020991cff4c4ab249697b
marioramirezlopez98-del <marioramirez.lopez98@gmail.com>
2026-04-14 13:31:07 +0000

    Desarrollo de proyecto Nombres de Dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let articulo = ['el', 'nuestro', 'su']
+  let adjetivo = ['gran', 'asombroso', 'pequeño', 'espectacular']
+  let sustantivo = ['chef', 'restaurante', 'lobo', 'coliseo']
+  let extensiones = ['.com', '.net', '.co', '.us']
+
+  const dominios = []
+
+  articulo.forEach(art => {
+    adjetivo.forEach(adj => {
+      sustantivo.forEach(sust => {
+        extensiones.forEach(ext => {
+          dominios.push(`Tu dominio es: ${art}${adj}${sust}${ext}`)
+        })
+      })
+    })
+  })
+  let domtrans = dominios.map(n => {
+    return `
+      <li class="list-group-item">${n}</li>
+    `
+  })
+
+  document.getElementById("generate").innerHTML = domtrans.join('')
+
+}
